4b7f017d13c72e0b16a34d5976d46a1c026cf40c
animalman33 <49952471+animalman33@users.noreply.github.com>
2023-08-30 20:27:59 -0400

    remove ocaml mode

--- init.el
+++ init.el
@@ -431,17 +431,4 @@
 (defun prev-match () (interactive nil) (next-match -1))
 (global-set-key [(shift f3)] 'prev-match)
 (global-set-key [backtab] 'auto-complete)
-;; OCaml configuration
-;;  - better error and backtrace matching
 
-(defun set-ocaml-error-regexp ()
-  (set
-   'compilation-error-regexp-alist
-   (list '("[Ff]ile \\(\"\\(.*?\\)\", line \\(-?[0-9]+\\)\\(, characters \\(-?[0-9]+\\)-\\([0-9]+\\)\\)?\\)\\(:\n\\(\\(Warning .*?\\)\\|\\(Error\\)\\):\\)?"
-    2 3 (5 . 6) (9 . 11) 1 (8 compilation-message-face)))))
-
-(add-hook 'tuareg-mode-hook 'set-ocaml-error-regexp)
-(add-hook 'caml-mode-hook 'set-ocaml-error-regexp)
-;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-(require 'opam-user-setup   (expand-file-name "~/.config/emacs/opam-user-setup.el"))
-;; ## end of OPAM user-setup addition for emacs / base ## keep this line
